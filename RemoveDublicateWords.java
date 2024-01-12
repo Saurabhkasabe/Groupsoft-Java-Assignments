d76ce764f841eba5af8792a08507bc31847ef70d
Saurabhkasabe <156282037+Saurabhkasabe@users.noreply.github.com>
2024-01-12 13:18:16 +0530

    Add files via upload

---
+++ RemoveDublicateWords.java
@@ -0,0 +1,28 @@
+package week2.day3;
+
+public class RemoveDublicateWords {
+
+	public static void main(String[] args) {
+		 String text = "We learn Java basics as part of java sessions in java week1";
+			String word[] = text.split(" ");
+			for(int i=0; i<word.length;i++)
+			{
+				if(word[i].isEmpty())
+				{
+					continue;
+				}
+				for (int j=i+1;j<word.length;j++)
+				{
+					if(word[j].equalsIgnoreCase(word[i]))
+					{
+						word[j]=" ";
+					}
+				}
+			}
+			
+			String text1 = String.join(" ", word);
+			System.out.println(text1);
+		}
+	}
+
+
